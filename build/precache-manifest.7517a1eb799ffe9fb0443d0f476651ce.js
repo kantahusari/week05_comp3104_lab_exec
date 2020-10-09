44a315646a04f1246b2ee4fc8cbce94fbead78da
kantahusari <66349870+kantahusari@users.noreply.github.com>
2020-10-08 23:25:55 -0400

    added all new files

---
+++ build/precache-manifest.7517a1eb799ffe9fb0443d0f476651ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76537d865d7af71c7f7e",
+    "url": "/static/css/main.8ae91f20.chunk.css"
+  },
+  {
+    "revision": "76537d865d7af71c7f7e",
+    "url": "/static/js/main.76537d86.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "c767bf3bddf96d4baa579c4cdcefb940",
+    "url": "/index.html"
+  }
+];
